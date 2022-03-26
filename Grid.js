d7a6d9df472b5330dd4f90a1ac102bf0659d438a
Zoheb Ahmed <zohebcool1542@gmail.com>
2022-03-26 21:17:41 +0530

    merging tiles done

--- Grid.js
+++ Grid.js
@@ -15,6 +15,23 @@ export default class Grid {
       )
     })
   }
+  get cells() {
+    return this.#cells
+  }
+  get cellsByRow() {
+    return this.#cells.reduce((cellsGrid, cell) => {
+      cellsGrid[cell.y] = cellsGrid[cell.y] || []
+      cellsGrid[cell.y][cell.x] = cell
+      return cellsGrid
+    }, [])
+  }
+  get cellsByColumn() {
+    return this.#cells.reduce((cellsGrid, cell) => {
+      cellsGrid[cell.x] = cellsGrid[cell.x] || []
+      cellsGrid[cell.x][cell.y] = cell
+      return cellsGrid
+    }, [])
+  }
 
   get #emptyCells() {
     return this.#cells.filter((cell) => cell.tile == null)
@@ -30,10 +47,38 @@ class Cell {
   #x
   #y
   #tile
+  #mergeTile
   constructor(cellElement, x, y) {
     this.#cellElement = cellElement
     this.#x = x
     this.#y = y
+  }
+  get mergeTile() {
+    return this.#mergeTile
+  }
+  set mergeTile(value) {
+    this.#mergeTile = value
+    if (value == null) return
+    this.#mergeTile.x = this.#x
+    this.#mergeTile.y = this.#y
+  }
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    )
+  }
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) return
+    this.tile.value = this.tile.value + this.mergeTile.value
+    this.mergeTile.remove()
+    this.mergeTile = null
+  }
+  get x() {
+    return this.#x
+  }
+  get y() {
+    return this.#y
   }
   get tile() {
     return this.#tile
--- script.js
+++ script.js
@@ -6,3 +6,74 @@ const gameBoard = document.getElementById('board')
 const grid = new Grid(gameBoard)
 grid.randomEmptyCell().tile = new Tile(gameBoard)
 grid.randomEmptyCell().tile = new Tile(gameBoard)
+setupInput()
+console.log(grid.cellsByColumn)
+function setupInput() {
+  window.addEventListener('keydown', handleInput, { once: true })
+}
+
+async function handleInput(e) {
+  console.log(e.key)
+  switch (e.key) {
+    case 'ArrowUp':
+      await moveUp()
+      break
+    case 'ArrowDown':
+      await moveDown()
+      break
+    case 'ArrowLeft':
+      await moveLeft()
+      break
+    case 'ArrowRight':
+      await moveRight()
+      break
+    default:
+      setupInput()
+      return
+  }
+  //other code.
+  grid.cells.forEach((cell) => cell.mergeTiles())
+  setupInput()
+}
+
+function moveUp() {
+  return slideTiles(grid.cellsByColumn)
+}
+function moveDown() {
+  return slideTiles(grid.cellsByColumn.map((column) => [...column].reverse()))
+}
+function moveLeft() {
+  slideTiles(grid.cellsByRow)
+}
+function moveRight() {
+  slideTiles(grid.cellsByRow.map((row) => [...row].reverse()))
+}
+
+function slideTiles(cells) {
+  return Promise.all(
+    cells.flatMap((group) => {
+      const promises = []
+      for (let i = 1; i < group.length; i++) {
+        const cell = group[i]
+        if (cell.tile == null) continue
+        let lastValidCell
+        for (let j = i - 1; j >= 0; j--) {
+          const moveToCell = group[j]
+          if (!moveToCell.canAccept(cell.tile)) break
+          lastValidCell = moveToCell
+        }
+
+        if (lastValidCell != null) {
+          promises.push(cell.tile.waitForTransition())
+          if (lastValidCell.tile != null) {
+            lastValidCell.mergeTile = cell.tile
+          } else {
+            lastValidCell.tile = cell.tile
+          }
+          cell.tile = null
+        }
+      }
+      return promises
+    })
+  )
+}
